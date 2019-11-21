31011bf387a88b7eb734898b6967469647e8381b
Coco Li <kel28@uw.edu>
2019-11-21 13:48:03 -0800

    add files

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const cors = require('cors');
+
+app.use(cors());
+
+let userNum = 0;
+
+app.get("/", (req, res) => {
+    userNum += 1;
+    res.send(JSON.stringify({ number: userNum % 2 }));   
+});
+
+app.listen(3030, () => console.log("listening"))
