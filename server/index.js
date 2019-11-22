2dbea510fbf130dff5b8e4fe23229b4b18056a40
Coco Li <kel28@uw.edu>
2019-11-21 16:10:27 -0800

    test

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.use(cors());
 
 let userNum = 0;
 
-app.get("/redirect.html", (req, res) => {
+app.get("/search-system-redirect/redirect", (req, res) => {
     userNum += 1;
     res.send(JSON.parse.stringify({ number: userNum % 2 }));   
 });
